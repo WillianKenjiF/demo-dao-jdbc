06269afa4196035865a4c9c0b0de769429ab81a1
Willian Kenji <willian.kenjif@hotmail.com>
2026-09-07 17:25:33 -0300

    DepartmentDao implementation and test

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -118,6 +118,7 @@ public class SellerDaoJDBC implements SellerDao{
 					+ "FROM seller INNER JOIN department "
 					+ "ON seller.DepartmentId = department.Id "
 					+ "WHERE seller.Id = ?");
+			
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
